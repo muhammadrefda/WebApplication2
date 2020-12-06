22eedef563277bd93d1036cb57277598317db962
muhammad refda <muhammadrefda@gmail.com>
2020-12-07 00:58:13 +0700

    Add project files.

---
+++ WebApplication2/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public int EmployeeId { get; private set; }
+
+        // GET: Employee
+        //public ActionResult Index()
+        //{
+        //  return View();
+        //}
+
+        public ActionResult EmployeeInfo()
+        {
+            Employee employee = new Employee();
+            {
+                employee.EmployeeId = 1000;
+                employee.EmployeeName = "refda";
+                employee.EmployeeLocation = "JKT";
+
+            };
+
+            return View(employee);
+
+
+
+
+        }
+    }
+}
